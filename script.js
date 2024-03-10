0755b8b2fb7ad4b7b683ca73ae9bf39fca9aedbb
Raphael Schwalb <rsch0027@student.monash.edu>
2024-03-10 13:03:08 +1100

    Hero styling complete. Need to add functionality to buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of smooth scrolling for the 'GET IN TOUCH' link
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
