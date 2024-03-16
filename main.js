ee7b0f9922f8ee77ba2ef21e91566c627aa044a0
Raphael Schwalb <rsch0027@student.monash.edu>
2024-03-16 17:45:12 +1100

    structure done, just need to fix details

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const buttons = Array.from(document.querySelectorAll('.toggleButton'));
 buttons.forEach((button) => {
-    button.addEventListener("click", () => {
+    button.addEventListener('click', () => {
         const innerToggled = Array.from(button.querySelectorAll('.toggled'));
         innerToggled.forEach((toggled) => {
             toggled.classList.toggle('hidden');
         });
 
-        button.parentNode.nextElementSibling.classList.toggle('hidden');
-
-        Clipboard.nextElementSIbling.scrollINtoView()
+        button.nextElementSibling.classList.toggle('hidden');
     });
 })
